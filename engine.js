3614057ae608e93faf244892030f61b655fff5cc
devRSV <devRSV@d.c>
2023-12-03 19:34:14 +0200

    Replace document generation with adding to existing one

--- engine.js
+++ engine.js
@@ -1,20 +1,19 @@
 window.Results = function() { 
-    let x = parent.document.FormManyServ;
-    let rNuser = x.Nuser.value; //Количество клиентов
-    let rNquery = x.Nquery.value; //Количество запросов
-    let rLServ = x.LServ.value; //Количество серверов
-    let rFreq = x.Freq.value; //Тактовая частота процессора
-    let rChannelSpeed=x.ChannelSpeed.value; //Скорость канала
-    let rNTactPerOper=x.NTactPerOper.value; //Количество тактов при 1 операции
-    let rNOperPerReq=x.NOperPerReq.value; //Количество операций при 1 запросе
-    let rRazmPacReq=x.RazmPacReq.value; //Средний размер запроса до сервера
-    let rRazmPacAns=x.RazmPacAns.value; //Размер ответа клиенту
-    let rChannelType=x.ChannelType.value; //Тип канала
-    let rNumLine=x.NumLine.value; //Количество каналов
-    
-    parent.document.write("<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=charset=windows-1251\"></head><body  background=\"Images/background.gif\">"); 
-    parent.document.write("<p><blockquote><b> ПОЛУЧЕННЫЕ РЕЗУЛЬТАТЫ <br>");
-    parent.document.write("</b></blockquote></p>");
+    let parentElement = parent.document.FormManyServ;
+    let rNuser = parentElement.Nuser.value; //Количество клиентов
+    let rNquery = parentElement.Nquery.value; //Количество запросов
+    let rLServ = parentElement.LServ.value; //Количество серверов
+    let rFreq = parentElement.Freq.value; //Тактовая частота процессора
+    let rChannelSpeed = parentElement.ChannelSpeed.value; //Скорость канала
+    let rNTactPerOper = parentElement.NTactPerOper.value; //Количество тактов при 1 операции
+    let rNOperPerReq = parentElement.NOperPerReq.value; //Количество операций при 1 запросе
+    let rRazmPacReq = parentElement.RazmPacReq.value; //Средний размер запроса до сервера
+    let rRazmPacAns = parentElement.RazmPacAns.value; //Размер ответа клиенту
+    let rChannelType = parentElement.ChannelType.value; //Тип канала
+    let rNumLine = parentElement.NumLine.value; //Количество каналов
+     
+    parentElement.innerHTML += ("<p><blockquote><b> ПОЛУЧЕННЫЕ РЕЗУЛЬТАТЫ <br>");
+    parentElement.innerHTML += ("</b></blockquote></p>");
 
     let ReceiveSpeed;
     let SendSpeed;
@@ -36,29 +35,29 @@ window.Results = function() {
    let MuRecv = 1/TrecvQuery; //интенсивность приема
    let RoRecv = Lamda/(MuRecv); //Загруженность канала приема
    let UispRecv = Math.min(RoRecv*rNumLine,rNumLine)/rNumLine; //Коэффициэнт использования канала
-   parent.document.write("<ul><p></p><li>Интенсивность поступления заявок от пользователей (заявок/сек):   " + Lamda);
-   parent.document.write("<p></p><li>Время получения одной заявки (сек):   " + TrecvQuery);
-   parent.document.write("<p></p><li>Загруженность агрегированного канала приема (норма <1):   " + RoRecv);
-   parent.document.write("<p></p><li>Коэффициэнт использования агрегированного канала приема:   " + UispRecv);
+   parentElement.innerHTML += ("<ul><p></p><li>Интенсивность поступления заявок от пользователей (заявок/сек):   " + Lamda);
+   parentElement.innerHTML += ("<p></p><li>Время получения одной заявки (сек):   " + TrecvQuery);
+   parentElement.innerHTML += ("<p></p><li>Загруженность агрегированного канала приема (норма <1):   " + RoRecv);
+   parentElement.innerHTML += ("<p></p><li>Коэффициэнт использования агрегированного канала приема:   " + UispRecv);
    if (UispRecv>=1)
     {
-     parent.document.write("<p></p><li>Канал(ы) приема перегружен(ы)");
+     parentElement.innerHTML += ("<p></p><li>Канал(ы) приема перегружен(ы)");
     }
    else 
    {
     let NsrRecv = UispRecv/(1-UispRecv); //Средняя длина очереди
     TNRecv = TrecvQuery*NsrRecv*rNumLine; //считаем что одна заявка передается только по 1 каналу
-    parent.document.write("<p></p><li>Средняя длина очереди:   " + NsrRecv);
+    parentElement.innerHTML += ("<p></p><li>Средняя длина очереди:   " + NsrRecv);
     if (UispRecv<0.5)
     {
-     parent.document.write("<p></p><li>Неэффективное использование канал(ов) приема");
+     parentElement.innerHTML += ("<p></p><li>Неэффективное использование канал(ов) приема");
     }
     else
     {
-     parent.document.write("<p></p><li>Нагрузка на канал(ы) приема нормальная");
+     parentElement.innerHTML += ("<p></p><li>Нагрузка на канал(ы) приема нормальная");
     };
    };
-   parent.document.write("</ul><br><hr>");
+   parentElement.innerHTML += ("</ul><br><hr>");
    
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -68,9 +67,9 @@ window.Results = function() {
    let Ts = rNTactPerOper*rNOperPerReq*Ttact; //Вpемя обслуживания заявки процессором [сек]
    let Mu = 1/Ts; //Интенсивность обслуживания заявок сервером
    let RoServ = LamdaServ/(Mu*rLServ); //Вероятность загрузки сервера
-   parent.document.write("<ul><p></p><li>Интенсивность обработки заявок сервером (заявок/сек):   " + Mu);
-   parent.document.write("<p></p><li>Вероятность загрузки обслуживающего устройства (сервера):   " + RoServ);
-   parent.document.write("<p></p><li>Среднее время обслуживания заявки сервером (с):   " + Ts);
+   parentElement.innerHTML += ("<ul><p></p><li>Интенсивность обработки заявок сервером (заявок/сек):   " + Mu);
+   parentElement.innerHTML += ("<p></p><li>Вероятность загрузки обслуживающего устройства (сервера):   " + RoServ);
+   parentElement.innerHTML += ("<p></p><li>Среднее время обслуживания заявки сервером (с):   " + Ts);
    let Pzagr = rLServ*RoServ; 
    let K1 = 0.0;
    let Fact1 = 0.0;
@@ -87,47 +86,47 @@ window.Results = function() {
     K2=K2+(Math.pow(Pzagr,i))/Fact2;
    }
    let K = K1/K2;
-   //parent.document.write("<li><p></p>K1:   " +K1 );
-   //parent.document.write("<li><p></p>K2:   " +K2 );
-   //parent.document.write("<li><p></p>K:   " +K );
+   //parentElement.innerHTML += ("<li><p></p>K1:   " +K1 );
+   //parentElement.innerHTML += ("<li><p></p>K2:   " +K2 );
+   //parentElement.innerHTML += ("<li><p></p>K:   " +K );
    
   let C = (1-K)/(1-RoServ*K); //Быстродействие системы ???
-   //parent.document.write("<li><p></p>C:   " +C);
+   //parentElement.innerHTML += ("<li><p></p>C:   " +C);
    
   let w = C*RoServ/(1-RoServ); //Количество заявок в очереди
-  //parent.document.write("<li><p></p>w:   " +w );
+  //parentElement.innerHTML += ("<li><p></p>w:   " +w );
   let q = w+rLServ*RoServ;  //Количество заявок в системе сервера
-  //parent.document.write("<li><p></p>q:   " +q );
+  //parentElement.innerHTML += ("<li><p></p>q:   " +q );
   let Tw = (C/rLServ)*Ts/(1-RoServ); //Среднее время ожидания в очереди
-  //parent.document.write("<li><p></p>Tw:   " +Tw );
+  //parentElement.innerHTML += ("<li><p></p>Tw:   " +Tw );
   let Tq = Tw+Ts; //Среднее время нахождения на сервере
-  //parent.document.write("<li><p></p>Tq:   " +Tq );
+  //parentElement.innerHTML += ("<li><p></p>Tq:   " +Tq );
   let GTq = Ts/(rLServ*(1-RoServ))*(Math.sqrt(C*(2-C)+rLServ*rLServ*(1-RoServ)*(1-RoServ))); //отклонение времени нахождения заявки в системе
-  // parent.document.write("<li><p></p>GTq:   " +GTq );
+  // parentElement.innerHTML += ("<li><p></p>GTq:   " +GTq );
   let Gw = (1/(1-RoServ))*(Math.sqrt(C*RoServ*(1+RoServ-C*RoServ))); //отклонение количества элементов данных в очереди
-  // parent.document.write("<li><p></p>Gw:   " +Gw);
+  // parentElement.innerHTML += ("<li><p></p>Gw:   " +Gw);
   if (RoServ>=1)
     {
-     parent.document.write("<p></p><li>Cервер(ы) перегружен(ы)");
+     parentElement.innerHTML += ("<p></p><li>Cервер(ы) перегружен(ы)");
     }
    else 
    {
-    parent.document.write("<p></p><li>Среднее время, которое заявки должны ожидать в очереди сервера Tw (с):   " + Tw);
-    parent.document.write("<p></p><li>Среднее время, которое заявки  находятся в сервере вообще (с) Tq:   " + Tq);
-    parent.document.write("<p></p><li>Средняя длина очереди в сервер: w  " + w);
-    parent.document.write("<p></p><li>Среднее количество элементов данных в сервере q:   " + q);
-    parent.document.write("<p></p><li>Стандартное отклонение среднего времени нахождения заявки в сервере (с) GTq:   " +GTq );
-    parent.document.write("<p></p><li>Стандартное отклонение количества элементов данных в очереди Gw:  " + Gw);
+    parentElement.innerHTML += ("<p></p><li>Среднее время, которое заявки должны ожидать в очереди сервера Tw (с):   " + Tw);
+    parentElement.innerHTML += ("<p></p><li>Среднее время, которое заявки  находятся в сервере вообще (с) Tq:   " + Tq);
+    parentElement.innerHTML += ("<p></p><li>Средняя длина очереди в сервер: w  " + w);
+    parentElement.innerHTML += ("<p></p><li>Среднее количество элементов данных в сервере q:   " + q);
+    parentElement.innerHTML += ("<p></p><li>Стандартное отклонение среднего времени нахождения заявки в сервере (с) GTq:   " +GTq );
+    parentElement.innerHTML += ("<p></p><li>Стандартное отклонение количества элементов данных в очереди Gw:  " + Gw);
     if (RoServ<0.5)
     {
-     parent.document.write("<p></p><li>Неэффективное использование сервера(ов)");
+     parentElement.innerHTML += ("<p></p><li>Неэффективное использование сервера(ов)");
     }
     else
     {
-     parent.document.write("<p></p><li>Нагрузка на сервер(ы) нормальная");
+     parentElement.innerHTML += ("<p></p><li>Нагрузка на сервер(ы) нормальная");
     };
    };
-   parent.document.write("</ul><br><hr>");
+   parentElement.innerHTML += ("</ul><br><hr>");
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 
@@ -138,52 +137,52 @@ window.Results = function() {
   let uSend = 1/(rRazmPacAns/SendSpeed/rNumLine);      
   let pSend = (rRazmPacAns/ReceiveSpeed)*Lamda/rNumLine;
   let UispSend = Math.min(pSend,1); //Коэффициэнт использования агрегированного канала
-  parent.document.write("<ul><p></p><li>Интенсивность поступления заявок от сервера (заявок/сек):   " + LamdaSend);
-  parent.document.write("<p></p><li>Время передачи одной заявки (сек):   " + TSendQuery);
-  parent.document.write("<p></p><li>Загруженность агрегированного канала передачи: " + pSend);
-  parent.document.write("<p></p><li>Коеффициент использования агрегированного канала передачи: " + UispSend);
-  parent.document.write("<p></p><li>Интенсивность трафика передачи: " + uSend);
+  parentElement.innerHTML += ("<ul><p></p><li>Интенсивность поступления заявок от сервера (заявок/сек):   " + LamdaSend);
+  parentElement.innerHTML += ("<p></p><li>Время передачи одной заявки (сек):   " + TSendQuery);
+  parentElement.innerHTML += ("<p></p><li>Загруженность агрегированного канала передачи: " + pSend);
+  parentElement.innerHTML += ("<p></p><li>Коеффициент использования агрегированного канала передачи: " + UispSend);
+  parentElement.innerHTML += ("<p></p><li>Интенсивность трафика передачи: " + uSend);
   if (UispSend>=1)
   {
-    parent.document.write("<p></p><li>Канал(ы) передачи перегружен(ы)");
+    parentElement.innerHTML += ("<p></p><li>Канал(ы) передачи перегружен(ы)");
   }
   else 
   {
     let NsrSend=UispSend/(1-UispSend); //Средняя длина очереди
     TNSend=TSendQuery*NsrSend*rNumLine;
-    parent.document.write("<p></p><li>Средняя длина очереди:   " + NsrSend);
+    parentElement.innerHTML += ("<p></p><li>Средняя длина очереди:   " + NsrSend);
     if (UispSend<0.5)
     {
-      parent.document.write("<p></p><li>Неэффективное использование канал(ов) передачи");
+      parentElement.innerHTML += ("<p></p><li>Неэффективное использование канал(ов) передачи");
     }
     else
     {
-      parent.document.write("<p></p><li>Нагрузка на канал(ы) передачи нормальная");
+      parentElement.innerHTML += ("<p></p><li>Нагрузка на канал(ы) передачи нормальная");
     };
   };
-  parent.document.write("</ul><br><hr>");
+  parentElement.innerHTML += ("</ul><br><hr>");
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
   if ((UispSend>=1)||(RoServ>=1)||(UispRecv>=1))
   {
-    parent.document.write("<p></p><li>Пользователь будет ждать ответа с каждым запросом все дольше, поскольку СМО не справляется с таким потоком данных. Очередь запросов будет возрастать до тех пор пока не переполнится буффер. После чего запросы вообще начнут теряться. Необходимо изменить параметры системы, чтобы она справлась с нагрузкой. Иначе система очень быстро не все пользователи получат ответы из-за перегрузки СМО.<br>");
+    parentElement.innerHTML += ("<p></p><li>Пользователь будет ждать ответа с каждым запросом все дольше, поскольку СМО не справляется с таким потоком данных. Очередь запросов будет возрастать до тех пор пока не переполнится буффер. После чего запросы вообще начнут теряться. Необходимо изменить параметры системы, чтобы она справлась с нагрузкой. Иначе система очень быстро не все пользователи получат ответы из-за перегрузки СМО.<br>");
   }
   else 
   {
     let Tuser_wait = TrecvQuery+TNRecv+Tq+TSendQuery+TNSend;
-    parent.document.write("<p></p><li>Время, которое пользователь ждет ответа:  " + Tuser_wait);
+    parentElement.innerHTML += ("<p></p><li>Время, которое пользователь ждет ответа:  " + Tuser_wait);
     let Tuser_waitMin = TrecvQuery+Ts+TSendQuery;
-    parent.document.write("<p></p><li>Минимальное время, за которое пользователь получает ответ(без очереди):  " + Tuser_waitMin);
+    parentElement.innerHTML += ("<p></p><li>Минимальное время, за которое пользователь получает ответ(без очереди):  " + Tuser_waitMin);
   };
-  parent.document.write("<p></p><li>Cредняя скорость приема:  " + ReceiveSpeed*UispRecv);
-  parent.document.write("<p></p><li>Cредняя скорость передачи:  " + SendSpeed*UispSend);
+  parentElement.innerHTML += ("<p></p><li>Cредняя скорость приема:  " + ReceiveSpeed*UispRecv);
+  parentElement.innerHTML += ("<p></p><li>Cредняя скорость передачи:  " + SendSpeed*UispSend);
   if (rChannelType=="Symplex")
   {
-    parent.document.write("<p></p><li>Максимальная скорость приема:  " + ReceiveSpeed);
-    parent.document.write("<p></p><li>Максимальная скорость передачи:  " + SendSpeed);
+    parentElement.innerHTML += ("<p></p><li>Максимальная скорость приема:  " + ReceiveSpeed);
+    parentElement.innerHTML += ("<p></p><li>Максимальная скорость передачи:  " + SendSpeed);
   };
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
    
-  parent.document.write("</ul></body></html>");
+  parentElement.innerHTML += ("</ul>");
 }
